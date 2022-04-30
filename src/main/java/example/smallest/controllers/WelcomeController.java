be2c2da0fdf36f9de8777c8d2f39c52f145f2827
Davy Cheung <pedio@users.noreply.github.com>
2022-04-30 14:57:35 +0800

    Update WelcomeController.java

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -1,21 +1,16 @@
 package example.smallest.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class WelcomeController {
 	
-	@RequestMapping(method = RequestMethod.GET, produces = {"application/json"})
+	@GetMapping(path = "/")
 	public @ResponseBody String helloWorld() {
 		
 		//Flux
-		
-		
-		
-		
 		return "Hello world ---------------- Davy"; //"application/json" mean this is a text not a redirect
 	}
 }
